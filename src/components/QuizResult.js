e53b68cc27d60bba6bbe7c0e233ea5d622f6ef07
BapiMajumder1402 <nbbapim@gmail.com>
2023-03-29 12:55:15 +0530

    Quiz

---
+++ src/components/QuizResult.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import result from './quiz.module.css'
+
+function QuizResult(props) {
+  return (
+    <>
+    <div className={result.show_score}>
+        Your Score:{props.score}<br/>
+        Total Score:{props.totalScore}
+    </div>
+    <button className={result.next_button} onClick={props.tryAgain}>Try Again</button>
+    </>
+  )
+}
+
+export default QuizResult
